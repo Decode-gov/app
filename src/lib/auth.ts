89560e74bfb8d86f6825b17fedbdaaa93a658911
Adriano Silva <adriano.silva@hgs-adv.com.br>
2025-10-27 13:48:59 -0300

    corrige a forma de deletar o cookie de autenticação na função clearCookieSession

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,10 @@
-"use server"
 
 import { cookies } from "next/headers";
 
 export async function clearCookieSession() {
   try{
-    const cookieStore = cookies();
-    (await cookieStore).delete('authToken');
+    const cookieStore = await cookies();
+    cookieStore.delete('authToken');
   }
   catch (error) {
     console.error("Erro ao deletar o cookie de autenticação:", error);
