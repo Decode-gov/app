c28269db3f488ca82e06bc3434a78c66b4cf402d
Adriano Silva <adriano.silva@hgs-adv.com.br>
2025-10-27 09:04:07 -0300

    Atualiza middleware para corrigir a verificação de cookies de sessão, alterando a forma de acesso ao cookie 'authToken' e simplificando a lógica de redirecionamento.

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -2,15 +2,18 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const sessionCookie = request.cookies.get("authToken");
+  const sessionCookie = request.cookies.has("authToken");
   const pathname = request.nextUrl.pathname;
+
+  console.log(`Middleware: ${pathname}, Authenticated: ${sessionCookie}`);
+  
    // Se não tem cookie de sessão e não está na página de login, redireciona para login
-  if (!sessionCookie?.value && pathname !== '/login') {
+  if (!sessionCookie && pathname !== '/login') {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
   // Se tem cookie de sessão e está na página de login, redireciona para dashboard
-  if (sessionCookie?.value && pathname === '/login') {
+  if (sessionCookie && pathname === '/login') {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
