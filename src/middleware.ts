16ab8436e4232d31516b25bbe03229ffb26ee2a1
Adriano Silva <adriano.silva@hgs-adv.com.br>
2025-10-27 11:02:15 -0300

    Adiciona middleware para gerenciar redirecionamentos com base na verificação do cookie de sessão 'authToken'.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,16 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const sessionCookie = request.cookies.has("authToken");
+  const sessionCookie = request.cookies.get("authToken");
   const pathname = request.nextUrl.pathname;
 
-  console.log(`Middleware: ${pathname}, Authenticated: ${sessionCookie}`);
-  
-   // Se não tem cookie de sessão e não está na página de login, redireciona para login
-  if (!sessionCookie && pathname !== '/login') {
+  // Se não tem cookie de sessão e não está na página de login, redireciona para login
+  if (!sessionCookie?.value && pathname !== '/login') {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
   // Se tem cookie de sessão e está na página de login, redireciona para dashboard
-  if (sessionCookie && pathname === '/login') {
+  if (sessionCookie?.value && pathname === '/login') {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
@@ -22,6 +20,14 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public assets (png, jpg, jpeg, svg)
+     */
     '/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$).*)',
   ],
 };
